e56587e205cf98e125a426759d91894a7df94a58
Vanya <vanya21254321@gmail.com>
2019-09-26 21:33:18 +0300

    labs creaed, have some bugs to fix

---
+++ CSCourseLab1/CSCourseLab1/Program.cs
@@ -0,0 +1,230 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace Lab1Course
+{
+    internal class Program
+    {
+        public enum Tasks
+        {
+            First,
+            Second,
+            Third,
+            Fourth,
+            Fifth,
+            Sixth,
+            Seventh,
+            EighthEvenNumbers,
+            EightOddNumbers
+        };
+        
+        public static string getNumberFromConsole(string messageToShow)
+        {
+            string valueToReturn;
+            Console.WriteLine(messageToShow);
+            valueToReturn = Console.ReadLine();
+            return valueToReturn;
+        }
+        
+        public static bool checkTheNumberForCorrectInput(string numberToCheck)
+        {
+            for (int i = 0; i < numberToCheck.Length; i++)
+            {
+                if(!Char.IsDigit(numberToCheck[i]))
+                {
+                    Console.WriteLine("Please, enter correct number."); 
+                    return true;
+                }
+            }
+
+            return false;
+        }
+        
+        //1
+        public static int checkTheNumbeOfDigitsInNumber(string number)
+        {
+            return number.Length;
+        }
+        
+        //2
+        public static void createArrayFromNumber(ref byte[] arrayToCreate, string number)
+        {
+            for (byte i = 0; i < number.Length; i++)
+            {
+                arrayToCreate[i] = Byte.Parse(number[i].ToString());
+            }
+        }
+        
+        //3
+        public static float getArithmeticMeanByNumbers(byte[] arrayWithNumbers)
+        {
+            float sumOfArrayElements = 0;
+            for (byte i = 0; i < arrayWithNumbers.Length; i++)
+            {
+                sumOfArrayElements += arrayWithNumbers[i];
+            }
+
+            float result = sumOfArrayElements / arrayWithNumbers.Length;
+
+            return result;
+        }
+        
+        
+        //4
+        public static double getGeometricMeanByNumbers(byte[] arrayWithNumbers)
+        {
+            
+            
+            double productOfNumbers = 1;
+            for (byte i = 0; i < arrayWithNumbers.Length; i++)
+            {
+                productOfNumbers *= arrayWithNumbers[i];
+            }
+
+            double result = Math.Pow(productOfNumbers, (double)1 / arrayWithNumbers.Length);
+            return result;
+        }
+        
+        //5
+        public static long getFactorial(int number)
+        {
+            if (number <= 1)
+            {
+                return 1; 
+            }
+            else
+            {
+                return number * getFactorial(number - 1); 
+            }
+             
+        }
+
+        //6
+        public static long getSumOfEvenNumbers(int finishNumber)
+        {
+            long sumOfEvenNumbers = 0;
+            for (int i = 0; i < finishNumber; i++)
+            {
+                if (i%2 == 0)
+                {
+                    sumOfEvenNumbers += i;
+                }
+            }
+
+            return sumOfEvenNumbers;
+        }
+        
+        //7
+        public static long getSumOfOddNumbers(int finishNumber)
+        {
+            long sumOfOddNumbers = 0;
+            for (int i = 0; i < finishNumber; i++)
+            {
+                if (i%2 != 0)
+                {
+                    sumOfOddNumbers += i;
+                }
+            }
+
+            return sumOfOddNumbers;
+        }
+        
+        //8
+        public static long getSumOfEvenNumbers(int startNumber, int endNumber)
+        {
+            long sumOfEvenNumbers = 0;
+            for (int i = startNumber; i <= endNumber; i++)
+            {
+                if (i%2 == 0)
+                {
+                    sumOfEvenNumbers += i;
+                }
+            }
+
+            return sumOfEvenNumbers;
+        }
+        
+        public static long getSumOfOddNumbers(int startNumber, int endNumber)
+        {
+            long sumOfOddNumbers = 0;
+            for (int i = startNumber; i <= endNumber; i++)
+            {
+                if (i%2 != 0)
+                {
+                    sumOfOddNumbers += i;
+                }
+            }
+
+            return sumOfOddNumbers;
+        }
+
+        public static void showValue(string valuetoShow, Tasks numberOfTask)
+        {
+            Console.WriteLine($"{numberOfTask}: { valuetoShow }; ");
+        }
+
+        public static void showValue(byte[] arrayFromNumber, Tasks numberOfTask)
+        {
+            Console.WriteLine($"{numberOfTask}:");
+            for (byte i = 0; i < arrayFromNumber.Length; i++)
+            {
+                Console.WriteLine($"arrayFromNumber[{i}] = {arrayFromNumber[i]};");
+            }
+        }
+
+        public static void Main(string[] args)
+        {
+            
+            string stringValueN;
+            int digitsInNumber = 0;
+            
+            stringValueN = getNumberFromConsole("Please, enter number N(integer): ");
+            if (checkTheNumberForCorrectInput(stringValueN))
+            {
+                return;
+            }
+            
+            int parsedToIntValue = Int32.Parse(stringValueN);
+            
+            //1
+            digitsInNumber = checkTheNumbeOfDigitsInNumber(stringValueN);
+            showValue(digitsInNumber.ToString(), Tasks.First);
+            
+            //2
+            byte[] arrayFromNumber = new byte[digitsInNumber];
+            createArrayFromNumber(ref arrayFromNumber, stringValueN);
+            showValue(arrayFromNumber, Tasks.Second);
+            
+            //3
+            float arithmeticMeanFromNumber = getArithmeticMeanByNumbers(arrayFromNumber);
+            showValue(arithmeticMeanFromNumber.ToString(), Tasks.Third);
+            
+            //4
+            double geometricMeanFromNumber = getGeometricMeanByNumbers(arrayFromNumber);
+            showValue(geometricMeanFromNumber.ToString(), Tasks.Fourth);
+            
+            //5
+            long factorialFromNumber = getFactorial(parsedToIntValue);
+            showValue(factorialFromNumber.ToString(), Tasks.Fifth);
+            
+            //6
+            long sumOfEvenNumbers = getSumOfEvenNumbers(parsedToIntValue);
+            showValue(sumOfEvenNumbers.ToString(), Tasks.Sixth);
+            
+            //7
+            long sumOfOddNumbers = getSumOfOddNumbers(parsedToIntValue);
+            showValue(sumOfOddNumbers.ToString(), Tasks.Seventh);
+            
+            //8
+            int startNumberForRange = Int32.Parse(getNumberFromConsole("Please, enter starting number for range: ")); 
+            int endNumberForRange = Int32.Parse(getNumberFromConsole("Please, enter finishing number for range: "));
+
+            long sumOfEvenNumbersByRange = getSumOfEvenNumbers(startNumberForRange, endNumberForRange);
+            showValue(sumOfEvenNumbersByRange.ToString(), Tasks.EighthEvenNumbers);
+
+            long sumOfOddNumbersByRange = getSumOfOddNumbers(startNumberForRange, endNumberForRange);
+            showValue(sumOfOddNumbersByRange.ToString(), Tasks.EightOddNumbers);
+
+        }
+    }
+}
---
+++ CSCourseLab2/CSCourseLab2/Program.cs
@@ -0,0 +1,155 @@
+﻿using System;
+using System.Text;
+
+namespace TrueCourseLab2
+{
+    class Program
+    {
+        static string getStringFromConsole()
+        {
+            Console.WriteLine("Please, enter some string: ");
+            string stringFromConsole = Console.ReadLine();
+            return stringFromConsole;
+        }
+        
+        
+        //3
+        static byte findAmountOfRepeatingStringInAnotherString(string firstString, string secondString)
+        {
+            byte counterForSymbls;
+            byte amountOfRepeats = 0;
+            for (byte i = 0; i < firstString.Length-secondString.Length + 1; i++)
+            {
+                counterForSymbls = 0;
+                if (firstString[i] == secondString[0])
+                {
+                    for (byte j = i, a = 0; a < secondString.Length; j++, a++ )
+                    {
+                        if (firstString[j] == secondString[a])
+                        {
+                            counterForSymbls++;
+                        }
+                    }
+
+                    if (counterForSymbls == secondString.Length)
+                    {
+                        amountOfRepeats++;
+                    }
+                }
+            }
+
+            return amountOfRepeats;
+        }
+        
+        //1
+        static bool checkIfStringContainsEverySymbolOfAnotherString(ref string firstString, ref string secondString)
+        {
+            StringBuilder firstStringArray = new StringBuilder(firstString);
+            StringBuilder secondStringArray = new StringBuilder(secondString);
+            
+            //--
+            for (byte i = 0; i < firstStringArray.Length; i++)
+            {
+                for (byte j = 0; j < secondStringArray.Length; j++)
+                {
+                    if (firstStringArray[i] == '#')
+                    {
+                        continue;
+                    }
+                    else if(firstStringArray[i] == secondStringArray[j])
+                    {
+                        
+                        for (int k = i + 1; k < firstStringArray.Length; k++)
+                        {
+                            if (firstStringArray[k] == firstStringArray[i])
+                            {
+                                firstStringArray[k] = '#';
+                            }
+                            
+                        }
+                        
+                        firstStringArray[i] = '#';
+                        
+                        for (int p = j + 1; p < secondStringArray.Length; p++)
+                        {
+                            if (secondStringArray[p] == secondStringArray[j])
+                            {
+                                secondStringArray[p] = '#';
+                            }
+                            
+                        }
+                        secondStringArray[j] = '#';
+                    }
+                }
+            }
+            //--
+            
+            for (byte i = 0; i < firstStringArray.Length; i++)
+            {
+                if (firstStringArray[i] != '#')
+                {
+                    return false;
+                }
+            }
+            
+            for (byte i = 0; i < secondStringArray.Length; i++)
+            {
+                if (secondStringArray[i] != '#')
+                {
+                    return false;
+                }
+            }
+
+            return true;
+
+        }
+        
+        //2
+        static string changeAllSameSymbolsInFirstStringThatContainsSecondString(ref string firstString, ref string secondString)
+        {
+            StringBuilder firstStringArray = new StringBuilder(firstString);
+
+            for (byte i = 0; i < firstStringArray.Length; i++)
+            {
+                for (byte j = 0; j < secondString.Length; j++)
+                {
+                    if (firstStringArray[i] == '#')
+                    {
+                        continue;
+                    }else if (firstStringArray[i] == secondString[j])
+                    {
+                        for (int k = i + 1; k < firstStringArray.Length; k++)
+                        {
+                            if (firstStringArray[k] == firstStringArray[i])
+                            {
+                                firstStringArray[k] = '#';
+                            }
+                            
+                        }
+                        
+                        firstStringArray[i] = '#';
+                    }
+                }
+            }
+
+            return firstStringArray.ToString();
+        }
+
+        static void Main(string[] args)
+        {
+            string firstString = getStringFromConsole();
+            string secondString = getStringFromConsole();
+
+            bool satusOfContainingFirstStringEverySymbolOfAnotherString =
+                checkIfStringContainsEverySymbolOfAnotherString(ref firstString, ref secondString);
+            Console.WriteLine($"1.Status of repeating symbols in two strings: {satusOfContainingFirstStringEverySymbolOfAnotherString}");
+            
+            string changedFirstString = changeAllSameSymbolsInFirstStringThatContainsSecondString(ref firstString, ref secondString);
+            Console.WriteLine($"2.Changed first string: {changedFirstString}");
+            
+            byte amountOfRepeatsAnotherString = findAmountOfRepeatingStringInAnotherString(firstString, secondString);
+            Console.WriteLine($"3.Amount of repeats second string in first string: {amountOfRepeatsAnotherString}");
+            
+        }
+    }
+}
