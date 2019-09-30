58fdb5cd6adbe168e8c48335242c50369577dce4
Vanya <vanya21254321@gmail.com>
2019-09-30 22:15:12 +0300

    added files.

---
+++ CSCourseLab3/CSCourseLab3/Forms/SecondForm.cs
@@ -0,0 +1,40 @@
+namespace CSCourseLab3.Forms
+{
+    public class SecondForm : Shapes
+    {
+        public override void generateElementsOfArrayByRandomNumbers()
+        {
+            
+        }
+
+        public override void generateElementsOfArrayByRange(int startNumber, int finishNumber)
+        {
+            
+        }
+
+        public override void generateElementsOfArrayByAnySymbols()
+        {
+            
+        }
+        
+        public override void generateElementsOfArrayByEnglishAlphabetSymbols()
+        {
+            
+        }
+
+        public override int findMaxElementInTheAreaOfMatrix()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public override int findMinElementInTheAreaOfMatrix()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public override int findSymbolInTheAreaOfMatrix(char symbolToFind)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
